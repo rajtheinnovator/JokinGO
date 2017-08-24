c2a8c3b82bb5f0001568d553d0e15b3ae2f489c3
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2017-08-24 23:19:43 +0530

    test ad running sucessfully in the mainActivity

--- app/src/main/java/com/enpassio/jokingo/MainActivity.java
+++ app/src/main/java/com/enpassio/jokingo/MainActivity.java
@@ -7,15 +7,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.enpassio.jokesandroidlibrary.JokeActivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
-
+    private AdView mAdView;
     private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = getApplicationContext();
+        MobileAds.initialize(getApplicationContext(),
+                "ca-app-pub-6697507015098409/8796911875");
+
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 
     public void ok(View view) {
